114f66d4d71be48f8715eedf7d9de3525d3f2f0f
KoenVDZ <koenatwork@gmail.com>
2021-03-17 17:40:43 -0300

    Testimonies

---
+++ test.php
@@ -0,0 +1,85 @@
+<header id="header" class="header relative spacing-bottom">
+  <div class="container">
+    <div class="wrapper-toolbar" style="min-height: 40px;">
+      <div class="wrapper-toolbar" style="min-height: 40px;">
+        <nav id="toolbar">
+          <div class="navbar navbar-fixed-top  container">
+            <div class="navbar-inner">
+              <div class="container">
+                <a class="btn btn-navbar collapsed" data-toggle="collapse" data-target="#nav-toolbar">
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </a>
+                <div class="nav-collapse" id="nav-toolbar">
+                  <div class="mod-languages pull-right">
+
+                    <ul class="lang-inline" dir="ltr">
+                      <li>
+                        <a href="/marielaspoltore.com/index.php/en/blog-uk">
+                          <img src="/marielaspoltore.com/media/mod_languages/images/en_gb.gif"
+                            alt="English (United Kingdom)" title="English (United Kingdom)"> </a>
+                      </li>
+                    </ul>
+
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+    <div class="wrapper-logo-menu row ">
+      <div id="logo" class="span2"><a href="https://localhost/marielaspoltore.com/" class="image">
+          <h2>marielaspoltore.com</h2><img
+            src="https://localhost/marielaspoltore.com/templates/js_wylia2/images/teenstyle/logo.png" alt="" title="">
+        </a></div>
+      <div id="menu" class="span10">
+        <nav id="menu_primary" class="clearfix">
+          <div class="navbar navbar-inverse">
+            <div class="navbar-inner">
+              <a class="btn btn-navbar collapsed" data-toggle="collapse" data-target="#nav-menu">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </a>
+              <div class="nav-collapse" id="nav-menu">
+
+                <ul class="menu nav">
+                  <li class="item-105"><a href="/marielaspoltore.com/index.php/es/">Inicio</a></li>
+                  <li class="item-147"><a href="/marielaspoltore.com/index.php/es/sobre-mi">Sobre mi</a></li>
+                  <li class="item-151 deeper dropdown parent"><a href="#" class="heading dropdown-toggle">
+                      Terapias<b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                      <li class="item-152"><a
+                          href="/marielaspoltore.com/index.php/es/terapias/sanacion-de-utero">Sanación de útero</a></li>
+                      <li class="item-153"><a
+                          href="/marielaspoltore.com/index.php/es/terapias/terapia-menstrual">Terapia Menstrual</a></li>
+                      <li class="item-154"><a href="/marielaspoltore.com/index.php/es/terapias/terapia-floral">Terapia
+                          floral</a></li>
+                      <li class="item-155"><a href="/marielaspoltore.com/index.php/es/terapias/clases-de-yoga">Clases de
+                          Yoga</a></li>
+                    </ul>
+                  </li>
+                  <li class="item-134 deeper dropdown parent"><a class=" dropdown-toggle"
+                      href="/marielaspoltore.com/index.php/es/tienda2">Tienda<b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                      <li class="item-133"><a
+                          href="/marielaspoltore.com/index.php/es/tienda2/listado-de-categorias">Listado de
+                          categorías</a></li>
+                    </ul>
+                  </li>
+                  <li class="item-116 current active"><a href="/marielaspoltore.com/index.php/es/blog-es">Blog</a></li>
+                  <li class="item-117"><a href="/marielaspoltore.com/index.php/es/admin">Iniciar sesión</a></li>
+                </ul>
+
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </div>
+</header>
